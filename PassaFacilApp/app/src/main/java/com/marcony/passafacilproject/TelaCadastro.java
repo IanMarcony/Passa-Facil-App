fe2e39bd0ba5e04e46811a2a0cb0e4e482060801
PauloRicardo <paulo.zulu.ricardo@gmail.com>
2020-05-05 10:46:56 -0400

    Added login and registration screen

---
+++ PassaFacilApp/app/src/main/java/com/marcony/passafacilproject/TelaCadastro.java
@@ -0,0 +1,14 @@
+package com.marcony.passafacilproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+    }
+}
